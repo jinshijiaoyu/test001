51e9feb94cfc3be9e0e1c7128bc839b9bfebc01a
金石 <zihaoshuhan2019@163.com>
2019-03-30 16:22:53 +0800

    first commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by 金石 on 2019/3/30.
+//  Copyright © 2019年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by 金石 on 2019/3/30.
+//  Copyright © 2019年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
